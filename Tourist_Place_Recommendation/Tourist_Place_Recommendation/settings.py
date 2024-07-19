5e8e43ed73b66ba0843e650f2b4982076d666e41
shravan kumar <skcode75@gmail.com>
2024-07-19 10:44:41 +0530

    railway changes

--- Tourist_Place_Recommendation/Tourist_Place_Recommendation/settings.py
+++ Tourist_Place_Recommendation/Tourist_Place_Recommendation/settings.py
@@ -72,6 +72,20 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
